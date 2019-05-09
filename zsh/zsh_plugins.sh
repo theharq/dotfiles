d9440b00303a7c0f6299a7a9ebc5bae21f6d00db
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2019-05-09 17:03:04 +0200

    Install Antibody for zsh plugins

---
+++ zsh/zsh_plugins.sh
@@ -0,0 +1,4 @@
+source /Users/meli/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /Users/meli/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /Users/meli/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /Users/meli/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
