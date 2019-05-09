b7c3d3318da2d0f927290345e3beb3d2b4e15830
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2019-05-09 17:39:37 +0200

    Add sublime configuration

---
+++ sublime/install.sh
@@ -0,0 +1,6 @@
+# Install Package Control
+cp "$HOME/dotfiles/sublime/Package Control.sublime-package" "$HOME/Library/Application Support/Sublime Text 3/Installed Packages/Package Control.sublime-package"
+
+# Make Symlinks of configuration files
+ln -sf "$HOME/dotfiles/sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+ln -sf "$HOME/dotfiles/sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings"
