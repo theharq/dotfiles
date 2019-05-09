2004fa1bed3f1755e8aebaf82d276a444b910707
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2019-05-09 10:47:06 +0200

    Add vim pathogen installation and files chmod

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -9,3 +9,7 @@
 
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
---
+++ vim/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Vim pathogen
+# https://github.com/tpope/vim-pathogen
+# pathogen.vim makes it super easy to install plugins and runtime files in their own private directories.
+
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+  mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+  # clone dracula color scheme into vim
+  git clone git@github.com:dracula/vim.git ~/.vim/bundle/dracula-theme
+fi
+
+exit 0
