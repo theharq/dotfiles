b5a9f8354679729a67bb292943883312a92ece88
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2019-05-09 15:00:46 +0200

    Symlink VScode files

--- vim/install.sh
+++ vim/install.sh
@@ -4,6 +4,8 @@
 # https://github.com/tpope/vim-pathogen
 # pathogen.vim makes it super easy to install plugins and runtime files in their own private directories.
 
+echo "  Installing Vim packages."
+
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
   mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
--- vscode/install.sh
+++ vscode/install.sh
@@ -4,6 +4,7 @@
 #
 # This installs my extensions of VSCode.
 
+echo "  Installing VSCode packages."
 
 code --install-extension dracula-theme.theme-dracula
 code --install-extension eamodio.gitlens
@@ -14,3 +15,7 @@ code --install-extension ms-vscode.sublime-keybindings
 code --install-extension PeterJausovec.vscode-docker
 code --install-extension rebornix.ruby
 code --install-extension sleistner.vscode-fileutils
+ 
+ln -sf "$HOME/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User/settings.json"
+ln -sf "$HOME/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User/keybindings.json"
+ln -sf "$HOME/dotfiles/vscode/snippets/elixir.json" "$HOME/Library/Application Support/Code/User/snippets/elixir.json"
