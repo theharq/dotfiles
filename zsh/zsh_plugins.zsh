0f66f3665842a30cd13c4230ef8943ee6e6a1481
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2019-09-17 01:43:58 +0200

    Activate dynamic loading of zsh plugins

--- zsh/zsh_plugins.zsh
+++ zsh/zsh_plugins.zsh
@@ -1,8 +1,8 @@
 # Dynamic loading
-# source <(antibody init)
-# antibody bundle < ~/dotfiles/zsh/zsh_plugins.txt
+source <(antibody init)
+antibody bundle < ~/dotfiles/zsh/zsh_plugins.txt
 
 # Static loading
 # antibody bundle < ~/dotfiles/zsh/zsh_plugins.txt > ~/dotfiles/zsh/zsh_plugins.sh
 # More info: https://getantibody.github.io/usage/
-source ~/dotfiles/zsh/zsh_plugins.sh
+# source ~/dotfiles/zsh/zsh_plugins.sh
