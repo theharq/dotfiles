4b9d5cb3024d34e1d7339129a1e142987c332282
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2020-06-16 00:12:22 +0200

    Add direnv to the system

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,6 @@
 export EDITOR='subl'
 export GIT_EDITOR='vim'
+
+# Hook direnv into the shell
+# direnv can load and unload environment variables depending on the current directory.
+eval "$(direnv hook zsh)"
