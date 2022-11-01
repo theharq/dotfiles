9fdb2ebeafd202b9d6c16638d3c8d59e43991805
Hildebrando Rueda <hildebrando.rueda@gmail.com>
2022-11-01 21:37:04 +0100

    Update where asdf is located

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,4 +1,4 @@
 # Load asdf if it is present in the system
-if [[ -f "/usr/local/opt/asdf/asdf.sh" ]]; then
-  . "/usr/local/opt/asdf/asdf.sh"
+if [[ -f /opt/homebrew/opt/asdf/libexec/asdf.sh ]]; then
+  . /opt/homebrew/opt/asdf/libexec/asdf.sh
 fi
