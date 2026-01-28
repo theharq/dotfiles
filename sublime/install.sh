c7e15893f701391d2d584a252ea92454f525e380
Hildebrando Rueda <hildebrando.rueda@zendesk.com>
2026-01-28 16:12:49 +0100

    I no longer use sublime text

--- sublime/install.sh
+++
@@ -1,6 +0,0 @@
-# Install Package Control
-cp "$HOME/dotfiles/sublime/Package Control.sublime-package" "$HOME/Library/Application Support/Sublime Text/Installed Packages/Package Control.sublime-package"
-
-# Make Symlinks of configuration files
-ln -sf "$HOME/dotfiles/sublime/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User/Preferences.sublime-settings"
-ln -sf "$HOME/dotfiles/sublime/Package Control.sublime-settings" "$HOME/Library/Application Support/Sublime Text/Packages/User/Package Control.sublime-settings"
